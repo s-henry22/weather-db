7bd62501b10756e968e0b1d270940dd63b0e8488
Spencer Henry <spencerhenry94@gmail.com>
2021-04-02 19:49:26 -0400

    pulled and formated data from weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,30 @@
-var getWeather = function () {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=3ba48c8c92cc5333d9adf48136136baa").then(function(response){
+var searchBar = document.querySelector("#search-bar");
+
+
+
+searchBar.addEventListener('keypress', setQuery);
+
+function setQuery(event) {
+    if(event.keyCode == 13) {
+        //getResults(searchBar.value);
+        console.log(searchBar.value)
+    };
+};
+
+// Request data from Weather API
+var getWeatherData = function(cityName) {
+    // format weather API
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=3ba48c8c92cc5333d9adf48136136baa";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
 
-getWeather();
+  
+
+
+//getWeatherData("Toronto");
